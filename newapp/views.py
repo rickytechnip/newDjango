c7683fa9027454574f61a966842e6761f7e708ed
rickytechnip <rickytechnip@gmail.com>
2020-06-07 11:04:23 -0400

    extending template

--- newapp/views.py
+++ newapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
-def homePageView(request):
-    return HttpResponse('Hello,World!')
+class HomePageView(TemplateView):
+    template_name = 'newapp/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'newapp/about.html'
